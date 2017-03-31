4bb957c253248606359071195ea935ea8d83782f
lrks <yuke1222@gmail.com>
2017-04-01 03:03:36 +0900

    OK

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var HID = require('node-hid');
-//console.log(HID.devices());
-
 var scanner = new HID.HID(1204, 56929);
-console.log(scanner);
+var buffer = '';
 
 scanner.on("data", function(data) {
-	console.log(data);
+	var hex = data[2];
+	switch(hex) {
+	case 0x1e:
+	case 0x1f:
+	case 0x20:
+	case 0x21:
+	case 0x22:
+	case 0x23:
+	case 0x24:
+	case 0x25:
+	case 0x26:
+		buffer += String(hex - 0x1d);
+		break;
+	case 0x27:
+		buffer += '0';
+		break;
+	case 0x28:
+		console.log(buffer);
+		buffer = '';
+		break;
 });
