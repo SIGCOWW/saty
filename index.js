ab5056d252e7831b5647a34c1068e6488c8dc58f
mrks <yuke1222@gmail.com>
2017-04-02 04:22:43 +0900

    今回はここまで

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var VID = 1204;
 var PID = 56929;
 var scanner = require('./lib/scanner');
+var db = require('./lib/db');
 
+console.log(db.getBookList());
 scanner.scan(VID, PID, function(isdn) {
 	console.log(isdn);
+	console.log(db.getReceiptData(isdn));
+	console.log(db.getDisplayData(isdn));
 });
