0b7e4a07057b0afa42c1a7ca801f4781ce1dcd9a
mrks <yuke1222@gmail.com>
2017-04-02 02:07:59 +0900

    db.jsとreceipt.jsとdisplay.jsを作ろう

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
-var HID = require('node-hid');
-var scanner = new HID.HID(1204, 56929);
-var buffer = '';
+var VID = 1204;
+var PID = 56929;
+var scanner = require('./lib/scanner');
 
-scanner.on("data", function(data) {
-	var hex = data[2];
-	switch(hex) {
-	case 0x1e:
-	case 0x1f:
-	case 0x20:
-	case 0x21:
-	case 0x22:
-	case 0x23:
-	case 0x24:
-	case 0x25:
-	case 0x26:
-		buffer += String(hex - 0x1d);
-		break;
-	case 0x27:
-		buffer += '0';
-		break;
-	case 0x28:
-		console.log(buffer);
-		buffer = '';
-		break;
-	}
+scanner.scan(VID, PID, function(isdn) {
+	console.log(isdn);
 });
