1344060ca8fcdd070fe96a8dcf6237361ac5f975
lrks <yuke1222@gmail.com>
2017-04-02 18:44:30 +0900

    プリンタ部分完成

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var VID = 1204;
-var PID = 56929;
+'use strict';
+var SCANNER_VID = 1204;
+var SCANNER_PID = 56929;
+var PRINTER_IPADDR = '192.168.192.168';
+
 var scanner = require('./lib/scanner');
 var db = require('./lib/db');
+var printer = require('./lib/printer');
 
 console.log(db.getBookList());
-scanner.scan(VID, PID, function(isdn) {
-	console.log(isdn);
-	console.log(db.getReceiptData(isdn));
+scanner.scan(SCANNER_VID, SCANNER_PID, function(isdn) {
+	console.log('ISDN', isdn);
+
+	var receipt = db.getReceiptData(isdn);
+	printer.printReceipt(receipt, PRINTER_IPADDR);
+	db.writeReceiptLog(receipt);
+
 	console.log(db.getDisplayData(isdn));
 });
