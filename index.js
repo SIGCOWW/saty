e45694756ca5b579b6079f88b307ee0909fa4bc1
lrks <yuke1222@gmail.com>
2017-08-10 22:59:26 +0900

    iizo

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var scanner = require('./lib/scanner');
 var db = require('./lib/db');
 var Printer = require('./lib/printer');
 var Display = require('./lib/display');
-
+var exec = require('child_process').exec;
 
 // Initialize
-PRINTER_IPADDR = null;
+//PRINTER_IPADDR = null;
 //DISPLAY_PORT = '/tmp/ttyS0';
 var printer = new Printer(PRINTER_IPADDR);
 var display = new Display(DISPLAY_PORT);
@@ -19,13 +19,35 @@ var display = new Display(DISPLAY_PORT);
 // Scan
 scanner.scan(SCANNER_VID, SCANNER_PID, function(isdn) {
 	if (isdn === printer.DUMP_CODE) {
-		console.log("DUMP!");
 		printer.dump();
-		return
+		return;
+	}
+
+	if (isdn === printer.DATE_CODE) {
+		exec('sudo date -s "2017/08/11 09:55:00"');
+		return;
 	}
 
 	var receipt = db.getReceiptData(isdn);
+	if (receipt === null) {
+		return;
+	}
 	printer.printReceipt(receipt);
 	display.printReceipt(receipt);
 	db.writeReceiptLog(receipt);
 });
+
+/*
+// Test
+var counter = 75;
+function ppp() {
+	counter--;
+	console.log('Ato: ' + counter);
+	var code = '2784403994033';
+	var receipt = db.getReceiptData(code);
+	printer.printReceipt(receipt);
+	var id = setTimeout(ppp, 1000);
+	if (counter < 0) clearTimeout(id);
+}
+ppp();
+*/
