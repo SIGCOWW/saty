442b87249cb2fc7e2fc6f14199a81f5ed3c1f7c6
lrks <yuke1222@gmail.com>
2017-10-20 16:15:41 +0900

    俺、技術書典が終わったら作り直すんだ...

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 var SCANNER_VID = 1204;
 var SCANNER_PID = 56929;
+var TEC_VID = 1204;
+var TEC_PID = 56929;
 var PRINTER_IPADDR = '192.168.192.168';
 var DISPLAY_PORT = '/dev/serial0';
 
 var scanner = require('./lib/scanner');
 var db = require('./lib/db');
 var Printer = require('./lib/printer');
+var TecPrinter = require('./lib/tecprinter');
 var Display = require('./lib/display');
 var exec = require('child_process').exec;
 
@@ -14,40 +17,26 @@ var exec = require('child_process').exec;
 //PRINTER_IPADDR = null;
 //DISPLAY_PORT = '/tmp/ttyS0';
 var printer = new Printer(PRINTER_IPADDR);
+var tecprinter = new TecPrinter(0x08a6, 0x0041);
 var display = new Display(DISPLAY_PORT);
 
 // Scan
 scanner.scan(SCANNER_VID, SCANNER_PID, function(isdn) {
-	if (isdn === printer.DUMP_CODE) {
-		printer.dump();
-		return;
-	}
-
-	if (isdn === printer.DATE_CODE) {
-		exec('sudo date -s "2017/08/11 09:55:00"');
-		return;
-	}
+	if (isdn === printer.DUMP_CODE) { printer.dump(); return; }
 
 	var receipt = db.getReceiptData(isdn);
-	if (receipt === null) {
-		return;
-	}
+	if (receipt === null) { return; }
+
 	printer.printReceipt(receipt);
 	display.printReceipt(receipt);
-	db.writeReceiptLog(receipt);
+	db.writeReceiptLog(receipt, 'epson');
 });
 
-/*
-// Test
-var counter = 75;
-function ppp() {
-	counter--;
-	console.log('Ato: ' + counter);
-	var code = '2784403994033';
-	var receipt = db.getReceiptData(code);
+scanner.scan(TEC_VID, TEC_PID, function(isdn) {
+	var receipt = db.getReceiptData(isdn);
+	if (receipt === null) { return; }
+
 	printer.printReceipt(receipt);
-	var id = setTimeout(ppp, 1000);
-	if (counter < 0) clearTimeout(id);
-}
-ppp();
-*/
+	//display.printReceipt(receipt);
+	db.writeReceiptLog(receipt, 'tec');
+});
