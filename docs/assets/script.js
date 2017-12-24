5168f74a67a12309b7da9787b08b52c6e57ee195
mrks <yuke1222@gmail.com>
2017-12-25 00:39:29 +0900

    忘れ物がないか心配

---
+++ docs/assets/script.js
@@ -0,0 +1,154 @@
+function payment(items, method) {
+	function saty(param) {
+		var host = window.localStorage.getItem('saty');
+		if (host) $.get('http://' + host + '/', param);
+	}
+
+	var sum = 0;
+	$.each(items, function(i, arr) {
+		sum += arr['price'] * arr['amount'];
+	});
+
+	saty({'method':method, 'amount':sum});
+	if (method == 'wechat') {
+		alert('売り子へ: 例の画像を見せてください。');
+		return;
+	}
+
+	var uri;
+	var keys = [ 100, 'q', 1500 ];
+	if (method == 'square') {
+		var appid = window.localStorage.getItem('square');
+		if (!appid) {
+			alert('Squareの設定がない');
+			return;
+		}
+
+		var param = {
+			'amount_money': {
+    			'amount': sum,
+				'currency_code': 'JPY'
+			},
+			'callback_url': 'https://sigcoww.github.io/saty/',
+			'client_id': appid,
+			'version': '1.3',
+			'options' : {
+				'supported_tender_types': ['CREDIT_CARD', 'CASH', 'OTHER', 'SQUARE_GIFT_CARD', 'CARD_ON_FILE']
+			}
+		};
+
+		uri = 'square-commerce-v1://payment/create?data=' + encodeURIComponent(JSON.stringify(param));
+	} else if (method == 'pxvpay') {
+		//keys.push('w');
+		//keys.push(1000);
+		$.each(items, function(i, arr) {
+			var key = 'asdfghjkl'[arr['index']];
+			for (var i=0; i<arr['amount']; i++) {
+				keys.push(key);
+				keys.push(50);
+			}
+		});
+		keys.push('e');
+		uri = 'serval://';
+	}
+
+	saty({'keys[]': keys});
+	setTimeout(function() {
+		window.location = window.location;
+		alert("キャンセル処理を収めた");
+	}, 100);
+	window.location = uri;
+}
+
+
+$(function(){
+	var param = null;
+	try {
+		param = JSON.parse(decodeURIComponent(location.search.replace('?data=', '')));
+	} catch (e) { console.log('nice catch'); }
+
+	if (param && param['status'] == 'error') {
+		alert('Square: ' + param['error_code'])
+	}
+
+
+	var TIMEOUT, SAMPLE_POS = 0, SAMPLE_IMG = window.localStorage.getItem('sample');
+	if (!SAMPLE_IMG) {
+		function kick() { };
+	} else {
+		function kick() {
+			if (TIMEOUT) {
+				clearTimeout(TIMEOUT);
+				INTERVAL = null;
+				TIMEOUT = null;
+				$("#sample").hide();
+			}
+
+			TIMEOUT = setTimeout(function() {
+				var img = new Image();
+				img.onload = function() {
+					$('#sample').width(img.width * (window.innerHeight / img.height) + window.innerWidth);
+					$('#sample').height(window.innerHeight);
+					$("#sample").show();
+				}
+				img.src = SAMPLE_IMG;
+			}, 60000);
+		}
+
+		$('#sample').css('background-image', 'url('+SAMPLE_IMG+')');
+		$('#sample').hammer().on('tap', function() { kick(); });
+		kick();
+	}
+
+	$("article").hammer().on('tap', function() {
+		kick();
+		var amount = $(this).find(".amount");
+		if (amount.length > 0) {
+			var val = parseInt(amount.text());
+			amount.text(val + 1);
+		} else {
+			$("section#methods article").each(function(i, elem) {
+				if ($(elem).hasClass('active')) $(elem).removeClass('active');
+			});
+		}
+
+		if (!$(this).hasClass('active')) $(this).addClass('active');
+	});
+
+	$("article").hammer().on('press', function() {
+		kick();
+		var amount = $(this).find(".amount");
+		if (amount.length > 0) amount.text("0");
+		$(this).removeClass('active');
+	});
+
+
+	$("html").hammer().data('hammer').get('swipe').set({direction: Hammer.DIRECTION_ALL});
+	$("html").hammer().on('swipe', function(ev) {
+		if (!$(this).is(ev.target)) return true;
+		kick();
+
+		var items = [];
+		$("section#items article").each(function(i, elem) {
+			var amount = parseInt($(elem).find('.amount').text());
+			if (amount > 0) {
+				items.push({
+					'title': $(elem).find('.title').text(),
+					'price': parseInt($(elem).find('.price').text()),
+					'amount': amount,
+					'index': i
+				});
+			}
+		});
+		var method = $("section#methods article.active").prop('id');
+
+		if (items.length == 0 || !method) {
+			alert('支払い情報不足');
+			return true;
+		}
+
+		$(".amount").text('0');
+		$("article").removeClass('active');
+		payment(items, method);
+	});
+});
